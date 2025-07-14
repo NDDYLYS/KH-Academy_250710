5f4356458d2ba0e4994c96de1146b2b24bda3813
NDDYLYS <nodvic@naver.com>
2025-07-14 10:26:05 +0900

    12

--- c250714/src/c250714_1/A_2.java
+++
@@ -1,8 +0,0 @@
-package c250714_1;
-
-public class A_2 
-{
-	public static void main(String[] args) 
-	{
-	}
-}
---
+++ c250714/src/c250714_1/t실수.java
@@ -0,0 +1,18 @@
+package c250714_1;
+
+public class t실수 
+{
+	public static void main(String[] args) 
+	{
+		// 실스 : 소숫점이 있는 수
+		
+		float a = 1.234f;
+		double b = 1.2345;
+		
+		// double이 기본값, float은 f로 구분
+		// 큰 값은 버린다
+		
+		
+		
+	}
+}
