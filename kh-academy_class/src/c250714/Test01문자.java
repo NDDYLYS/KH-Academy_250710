f7a773743a68b43fc9b53cf5ee8fd724af2a2bf4
NDDYLYS <nodvic@naver.com>
2025-07-15 08:56:24 +0900

    ddd

--- kh-academy_class/src/c250714/Test01문자.java
+++ kh-academy_class/src/c250714/Test01문자.java
@@ -4,6 +4,20 @@ public class Test01문자
 {
 	public static void main(String[] args) 
 	{
+		char a = 65;
+		System.out.println(a);
+		
+		char b = 'B';
+		System.out.println(b);
+		
+		char c = '가';
+		char d = '힣';
+		
+		System.out.println(c);
+		System.out.println(d);
+		
+		System.out.println((int)c);
+		System.out.println((int)d);
 		
 	}
 }
---
+++ kh-academy_class/src/c250714/Test02문자열.java
@@ -0,0 +1,30 @@
+package c250714;
+
+public class Test02문자열 
+{
+	public static void main(String[] args) 
+	{
+			String a = "Hello";
+			
+			System.out.println(a);
+			System.out.println(a.length());
+			System.out.println(a.toUpperCase());
+			System.out.println(a.toLowerCase());
+			
+			String b = "hello" + 100;
+			System.out.println(b);
+			
+			String d = "피자  떡볶이";
+			String e = "피자\t떡볶이";
+			System.out.println(d);
+			System.out.println(e);
+			
+			String f = "피자\n떡볶이";
+			System.out.println(f);
+			
+			String g = "나는 오늘 \"피자\"를 먹을거야";
+			System.out.println(g);
+			
+			이윤석(890919) 시청 완료
+	}
+}
