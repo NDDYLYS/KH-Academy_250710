97363e04bd6b12d2655ee2c3c885c8afa1110284
NDDYLYS <nodvic@naver.com>
2025-07-14 12:00:17 +0900

    1

--- c250714/src/c250714/bmi.java
+++ c250714/src/c250714/bmi.java
@@ -4,11 +4,12 @@ public class bmi
 {
 	public static void main(String[] args) 
 	{
-		float tall = 1.8f;
-		float weight = 80f;
+		float tall = 1.711f;
+		float weight = 53f;
 		
-		float bmi = weight / (tall * tall);
+		float bmi = weight / (float)Math.pow(tall, 2);
 		
-		System.out.println("키는 " + tall + "cm이며 체중은 " + weight + "kg인 사람의 BMI 수치는" + bmi + "입니다.");
+		System.out.println("키는 " + (int)(tall * 100) + "cm이며 체중은 " + weight + 
+				"kg인 사람의 BMI 수치는" + bmi + "입니다.");
 	}
 }
