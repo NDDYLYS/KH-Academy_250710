dff94342747895941255918ae64db47d3681c118
NDDYLYS <nodvic@naver.com>
2025-07-15 15:33:48 +0900

    다중조건의 탄생배경

---
+++ kh-academy_class/src/c250715io/Test04다중조건.java
@@ -0,0 +1,17 @@
+package c250715io;
+
+public class Test04다중조건 
+{
+	public static void main(String[] args) 
+	{
+		// 월 정보를 이용하여 계절을 출력하세요
+		
+		int month = 3;
+		
+		if () 
+		{
+			System.out.println();	
+		}
+
+	}
+}
