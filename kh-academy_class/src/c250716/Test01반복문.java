b650a355a35c410e44c16a9b11e58ebb4753e1a0
NDDYLYS <nodvic@naver.com>
2025-07-16 12:41:50 +0900

    aaa

---
+++ kh-academy_class/src/c250716/Test01반복문.java
@@ -0,0 +1,21 @@
+package c250716;
+
+public class Test01반복문 
+{
+	public static void main(String[] args) 
+	{
+		// HelloJava를 10번 출력하기
+		// 비어있는 변수 i를 준비하고 	선언부
+		// 1씩 증가시키며 			증감부
+		// 10이 되기 전까지만 실행 	조건부
+		for (int i = 0; i < 5; i++)
+		{
+			System.out.println("KH정보교육원");
+		}
+		
+		for (int i = 0; i < 30; i++)
+		{
+			System.out.println("100% 취업 화이팅");
+		}
+	}
+}
